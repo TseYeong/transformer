567a68916b8d5e438d68435f2f7a2babff775907
TseYeong <xieyun220823@gmail.com>
2025-06-20 22:25:15 +0800

    Base Attention Class

---
+++ src/layers/attention/attention.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+from abc import ABC, abstractmethod
+
+
+class BaseAttention(nn.Module, ABC):
+    def __int__(self, d_model: int, n_heads: int, dropout: int = 0.1):
+        super().__int__()
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.dropout = dropout
+        self.d_head = d_model // n_heads
+        assert d_model % n_heads == 0
+
+        self.q_proj = nn.Linear(d_model, d_model)
+        self.k_proj = nn.Linear(d_model, d_model)
+        self.v_proj = nn.Linear(d_model, d_model)
+        self.o_proj = nn.Linear(d_model, d_model)
+
+    @abstractmethod
+    def forward(self, x_q, x_kv):
+        """
+        :param x_q: [B, Lq, D]
+        :type x_q: torch.Tensor
+        :param x_kv: [B, Lkv, D] or None (self-attn)
+        :type x_kv: Union[torch.Tensor | None]
+        :return: [B, Lq, D]
+        :rtype: torch.Tensor
+        """
+        pass
+
+    def _reshape_qkv(self, x_q: torch.Tensor, x_kv: torch.Tensor):
+        B, Lq, _ = x_q.size()
+        Lkv = x_kv.size(1)
+
+        Q = self.q_proj(x_q).view(B, Lq, self.n_heads, self.d_head).transpose(1, 2)
+        K = self.k_proj(x_kv).view(B, Lkv, self.n_heads, self.d_head).transpose(1, 2)
+        V = self.v_proj(x_kv).view(B, Lkv, self.n_heads, self.d_head).transpose(1, 2)
+
+        return Q, K, V
